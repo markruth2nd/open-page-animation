3d4358512315e5f5b1efba945d0d61707e238d61
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-05-19 15:18:10 +0100

    added first part of hero animation, seems to be working smoothly

---
+++ app.js
@@ -0,0 +1,12 @@
+/* Below I am going to use doc.queryselector to select the relevant parts to this animated page which are all going to take part in some form */
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('#logo');
+const hamburger = document.querySelector('.hamburger');
+const headline = document.querySelector('.headline');
+
+/* now I will create a timeline */
+const tl = new TimelineMax();
+
+/* Now I am going to define the elements that I want to animate, add the duration of the animation and then in the first {object} I am going to define the start state of the animation and then in the second {object} I will define the ending state of the animation, so {from},{To}*/
+tl.fromTo(hero,1.4, {height: "0%"}, {height: "80%", ease: Power2.easeInOut}) /* addint the ease to the {To} part of the fromTo timeline ensures a smoother transition of the hero animation */
